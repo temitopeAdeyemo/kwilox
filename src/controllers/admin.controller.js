e13869caed84c26aa1fb3f85b03555b8c9120b2d
temitopeAdeyemo <temitopejulius9@gmail.com>
2022-03-01 15:43:51 +0100

    feat/I just added a readme file

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,4 +1,4 @@
-const Admin = require("../models/Admin.model");
+const Admin = require("../models/admin.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv").config();
